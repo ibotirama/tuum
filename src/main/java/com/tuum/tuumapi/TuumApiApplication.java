4e3f54782c2829b8a890f1c145e3629131751240
Pedro Andrade <pedro.henrique1@gmail.com>
2022-04-15 00:54:27 +0300

    first commit

---
+++ src/main/java/com/tuum/tuumapi/TuumApiApplication.java
@@ -0,0 +1,13 @@
+package com.tuum.tuumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuumApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuum/tuumapi/TuumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuum.tuumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
